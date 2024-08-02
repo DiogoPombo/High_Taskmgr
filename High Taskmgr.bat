dece4ab62b0bfb5efca3a4e72aee95aee197f6ce
DiogoPombo <150866276+DiogoPombo@users.noreply.github.com>
2024-08-02 00:59:30 -0300

    Add files via upload

---
+++ High Taskmgr.bat
@@ -0,0 +1,12 @@
+@echo off
+start /realtime taskmgr
+
+set APP=High Taskmgr
+set AUTHOR=POMBO
+set AVATAR=\Õ/
+set MADE_BY=MADE BY:
+set SPACE= 
+set KEY=EWEP
+echo %APP%%SPACE%%MADE_BY%%SPACE%%SPACE%%AUTHOR%%SPACE%%SPACE%%AVATAR%%SPACE%%KEY%
+
+exit
